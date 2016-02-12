8dd4b92ddb72379a4d8c4a3f067487cc57dc2b31
joetinoco <jose.tinoco@gmail.com>
2016-02-11 21:26:36 -0500

    express.js 'compress' correction

--- config/express.js
+++ config/express.js
@@ -9,7 +9,7 @@ module.exports = function(){
   if(process.env.NODE_ENV === 'development'){
     app.use(morgan('dev'));
   } else if (process.env.NODE_ENV === 'production'){
-    app.use(compress());
+    app.use(express.compress());
   }
 
   app.use(express.static('public/'));
